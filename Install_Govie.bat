2b5b315e92539b5ede92877169141fb4d34657de
Lorenz Wieseke <lorenz.wieseke@3dit.de>
2020-12-04 15:02:51 +0100

    fixing whitespace at end of blenderpath

--- Install_Govie.bat
+++ Install_Govie.bat
@@ -6,12 +6,16 @@ set prefix=C:\Program Files\Blender Foundation\Blender 2.
 set blenderpath=
 
 for %%v in (%version_list%) do (
-    set "full_path=%prefix%%%v"
+    set full_path=%prefix%%%v
     if exist !full_path! (set blenderpath=!full_path! & echo "Found Blender Version %%v")
 )
 
-c:
-cd %blenderpath%
+set blenderpath=%blenderpath:~0,-1%
+set PATH=%PATH%;%blenderpath%
+
+echo %blenderpath%
+
+
 blender -b -P .\Addon\install_addon.py -- -addon_path .\Addon\GovieTools.zip -addon_name GovieTools
 blender -b -P .\Addon\install_addon.py -- -addon_path .\Addon\GLBTextureTools.zip -addon_name GLBTextureTools
 
